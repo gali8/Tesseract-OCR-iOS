ee8c951d867b70c7c5cf58b9c6ab89217d4b23b0
Nikolay Volosatov <bamx23@yandex-team.ru>
2014-12-15 14:21:42 +0300

    tests-fix: Add empty testing project

---
+++ TestsProject/TestsProject/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  TestsProject
+//
+//  Created by Nikolay Volosatov on 15.12.14.
+//  Copyright (c) 2014 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+
+    self.window.rootViewController = [[ViewController alloc] init];
+
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
