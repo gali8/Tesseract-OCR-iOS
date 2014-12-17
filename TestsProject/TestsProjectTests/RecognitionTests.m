6a68b6a4a0cffad1cca938410e7250f48cb9f878
Nikolay Volosatov <bamx23@yandex-team.ru>
2014-12-17 12:34:45 +0300

    thresholding: Add test for thresholded image fetching

--- TestsProject/TestsProjectTests/RecognitionTests.m
+++ TestsProject/TestsProjectTests/RecognitionTests.m
@@ -10,6 +10,8 @@
 #import <TesseractOCR/TesseractOCR.h>
 #import <Kiwi/Kiwi.h>
 
+#import "UIImage+G8Equal.h"
+
 static NSTimeInterval const kG8MaximumRecognitionTime = 5.0;
 static NSString *const kG8Languages = @"eng+ita";
 static NSString *const kG8WhiteList = @"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -30,7 +32,7 @@ void (^wait)(NSTimeInterval, BOOL (^)()) = ^(NSTimeInterval maximumWait, BOOL (^
 __block G8Tesseract *tesseract;
 
 let(image, ^id{
-    return nil;
+    return [UIImage imageNamed:@"image_sample.jpg"];
 });
 
 void (^recognizeImage)() = ^{
@@ -66,10 +68,6 @@ void (^recognizeImageUsingOperation)() = ^{
 
 describe(@"Simple numbers", ^{
 
-    let(image, ^id{
-        return [UIImage imageNamed:@"image_sample.jpg"];
-    });
-
     it(@"Should recognize", ^{
         recognizeImage();
 
@@ -81,16 +79,30 @@ describe(@"Simple numbers", ^{
 
 describe(@"NSOperation usage", ^{
 
-    let(image, ^id{
-        return [UIImage imageNamed:@"image_sample.jpg"];
-    });
-
     it(@"Should recognize", ^{
         recognizeImageUsingOperation();
 
         NSString *recognizedText = [tesseract recognizedText];
         [[recognizedText should] containString:@"1234567890"];
     });
+
+});
+
+describe(@"Thresholding", ^{
+
+    it(@"Should fetch thresholded image", ^{
+        G8Tesseract *tesseract = [[G8Tesseract alloc] initWithLanguage:kG8Languages];
+        tesseract.image = image;
+
+        UIImage *thresholded = tesseract.thresholdedImage;
+
+        [[thresholded shouldNot] beNil];
+
+        BOOL isEqual = [tesseract.thresholdedImage g8_isEqualToImage:[UIImage imageNamed:@"image_sample_tr"]];
+
+        [[theValue(isEqual) should] beYes];
+    });
+
 });
 
 SPEC_END
---
+++ TestsProject/TestsProjectTests/UIImage+G8Equal.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+G8Equal.h
+//  TestsProject
+//
+//  Created by Nikolay Volosatov on 17.12.14.
+//  Copyright (c) 2014 Daniele Galiotto - www.g8production.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (G8Equal)
+
+- (BOOL)g8_isEqualToImage:(UIImage*)image;
+
+@end
