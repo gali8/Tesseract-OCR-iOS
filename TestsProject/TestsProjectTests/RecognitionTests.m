e87e434f8cd2d1e539cbc0433dd46f11a1e56cd3
Kirill Makankov <kmakankov@griddynamics.com>
2015-01-06 12:24:21 +0300

    Moved hOCR tests back to recognitionTests.m

--- TestsProject/TestsProjectTests/RecognitionTests.m
+++ TestsProject/TestsProjectTests/RecognitionTests.m
@@ -240,4 +240,45 @@ describe(@"Well scaned page", ^{
     });
 });
 
+#pragma mark - hOCR
+
+describe(@"hOCR", ^{
+    
+    it(@"Should sample image", ^{
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"image_sample" ofType:@"hOCR"];
+        
+        helper.image = [UIImage imageNamed:@"image_sample.jpg"];
+        helper.charWhitelist = @"0123456789";
+        
+        [helper recognizeImage];
+        NSString *hOCR = [helper.tesseract recognizedHOCRForPageNumber:0];
+        
+        NSError *error = nil;
+        [[hOCR should] equal:[NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error]];
+        NSAssert(error == nil, @"error loading hOCR from file %@: %@", path, error);
+    });
+    
+    it(@"Should well scanced page", ^{
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"well_scaned_page" ofType:@"hOCR"];
+        
+        helper.image = [UIImage imageNamed:@"well_scaned_page"];
+        
+        [helper recognizeImage];
+        NSString *hOCR = [helper.tesseract recognizedHOCRForPageNumber:0];
+        
+        NSError *error = nil;
+        [[hOCR should] equal:[NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error]];
+        NSAssert(error == nil, @"error loading hOCR from file %@: %@", path, error);
+    });
+    
+    it(@"Should return nil without prerecognition", ^{
+        G8Tesseract *tesseract = [[G8Tesseract alloc] init];
+        
+        NSString *hOCR = [tesseract recognizedHOCRForPageNumber:0];
+        [[hOCR should] beNil];
+    });
+});
+
 SPEC_END
